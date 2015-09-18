c2c343821216a6888ce51fdeadeb9fbe558d66cd
psm <362976241@qq.com>
2015-09-18 18:47:36 +0800

    asd

---
+++ MyWork01/src/com/nima/MyWork01/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nima.MyWork01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
